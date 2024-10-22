6502d169643ce80015382cee26d9bb1980eff09b
Joaquin Sosa <cto-joaquinsosa@hotmail.com>
2024-10-22 16:57:04 -0300

    test nav bar and form components

---
+++ frontend/src/components/UserComponent.vue
@@ -0,0 +1,28 @@
+<template>
+    <div class="container">
+        <form >
+            <div class="">
+                <label>Ingrese su correo electrónico</label>
+                <input type="text" placeholder="Email">
+            </div>
+            <div>
+                <label>Ingrese su contraseña</label>
+                <input type="text" placeholder="Password">
+            </div>
+            <button class="btn btn-danger">Ingresar</button>
+        </form>
+    </div>
+</template>
+<style scoped>
+.container {
+    margin: 25px;
+    padding: 10px;
+    background-color: gray;
+    border-radius: 15%;
+    width: 400px;
+    height: 300px;
+    align-items: center;
+    justify-self: center;
+    display: grid;
+}
+</style>
--- frontend/src/views/HomeView.vue
+++ frontend/src/views/HomeView.vue
@@ -1,18 +1,19 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <NavBar/>
+  <div class="container">
+    <UserComponent/>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+import NavBar from '@/components/NavBar.vue';
+import UserComponent from '@/components/UserComponent.vue';
 
 export default {
   name: 'HomeView',
   components: {
-    HelloWorld
-  }
+    NavBar,
+    UserComponent
+}
 }
 </script>
